45c2d705c6439e8edbae5cbde410d505436fe97f
Mariel de la Garza <mariel.code@gmail.com>
2024-09-25 13:20:40 -0400

    Working intro content grid

--- src/components/IntroContent.svelte
+++ src/components/IntroContent.svelte
@@ -25,30 +25,54 @@
             </span><span>More on this database</span></a
           >
         </p>
-      </div>
-    </div>
-    <!--GRID-->
-    <div class="intro-content__grid">
-      <div class="intro-content__grid-item">
-        <iframe src='https://flo.uri.sh/visualisation/19490897/embed' title='Interactive or visual content' class='flourish-embed-iframe' frameborder='0' scrolling='no' style='width:100%;height:600px;' sandbox='allow-same-origin allow-forms allow-scripts allow-downloads allow-popups allow-popups-to-escape-sandbox allow-top-navigation-by-user-activation'></iframe>
-      </div>
-      <div class="intro-content__grid-item">
-        <h1>2024</h1>
-        <h2>Biggest Year for Investment</h2>
-      </div>
-      <div class="intro-content__grid-item">
-        <iframe src='https://flo.uri.sh/visualisation/19489806/embed' title='Interactive or visual content' class='flourish-embed-iframe' frameborder='0' scrolling='no' style='width:100%;height:600px;' sandbox='allow-same-origin allow-forms allow-scripts allow-downloads allow-popups allow-popups-to-escape-sandbox allow-top-navigation-by-user-activation'></iframe>
-      </div>
-      <div class="intro-content__grid-item">
-        <h1>Renewable Energy</h1>
-        <h2>Sector with Greatest Investment</h2>
-      </div>
-      <div class="intro-content__grid-item">
-        <iframe src='https://flo.uri.sh/visualisation/19490010/embed' title='Interactive or visual content' class='flourish-embed-iframe' frameborder='0' scrolling='no' style='width:100%;height:600px;' sandbox='allow-same-origin allow-forms allow-scripts allow-downloads allow-popups allow-popups-to-escape-sandbox allow-top-navigation-by-user-activation'></iframe>
-      </div>
-      <div class="intro-content__grid-item">
-        <h1>$7.5 Billion</h1>
-        <h2>Single Largest Investment</h2>
+        <!--GRID-->
+        <div class="intro-content__grid">
+          <div class="intro-content__grid-item">
+            <iframe
+              src="https://flo.uri.sh/visualisation/19490897/embed"
+              title="Interactive or visual content"
+              class="flourish-embed-iframe"
+              frameborder="0"
+              scrolling="no"
+              style="width:100%;height:600px;"
+              sandbox="allow-same-origin allow-forms allow-scripts allow-downloads allow-popups allow-popups-to-escape-sandbox allow-top-navigation-by-user-activation"
+            ></iframe>
+          </div>
+          <div class="intro-content__grid-item">
+            <h1>2024</h1>
+            <h2>Biggest Year for Investment</h2>
+          </div>
+          <div class="intro-content__grid-item">
+            <iframe
+              src="https://flo.uri.sh/visualisation/19489806/embed"
+              title="Interactive or visual content"
+              class="flourish-embed-iframe"
+              frameborder="0"
+              scrolling="no"
+              style="width:100%;height:600px;"
+              sandbox="allow-same-origin allow-forms allow-scripts allow-downloads allow-popups allow-popups-to-escape-sandbox allow-top-navigation-by-user-activation"
+            ></iframe>
+          </div>
+          <div class="intro-content__grid-item">
+            <h1>Renewable Energy</h1>
+            <h2>Sector with Greatest Investment</h2>
+          </div>
+          <div class="intro-content__grid-item">
+            <iframe
+              src="https://flo.uri.sh/visualisation/19490010/embed"
+              title="Interactive or visual content"
+              class="flourish-embed-iframe"
+              frameborder="0"
+              scrolling="no"
+              style="width:100%;height:600px;"
+              sandbox="allow-same-origin allow-forms allow-scripts allow-downloads allow-popups allow-popups-to-escape-sandbox allow-top-navigation-by-user-activation"
+            ></iframe>
+          </div>
+          <div class="intro-content__grid-item">
+            <h1>$7.5 Billion</h1>
+            <h2>Single Largest Investment</h2>
+          </div>
+        </div>
       </div>
     </div>
   </main>
